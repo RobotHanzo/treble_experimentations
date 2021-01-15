356ecd70dc0bf18a0feb9a80a04f80beeeb9bcff
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2021-01-15 18:38:43 +0800

    Add an M for extra functionality

--- rr.sh
+++ rr.sh
@@ -67,6 +67,6 @@ echo ""
 echo "================================="
 export SSHPASS=$sftp_password
 sshpass -e sftp -oBatchMode=no -oPort=23 -b - $sftp_username@$sftp_host << !
-   $(for i in $images; do echo "put $i"; done)
+   mput $images
    bye
 !
