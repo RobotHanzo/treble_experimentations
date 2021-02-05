3e4c5243c75fdcb96fa9cab12579e9ba548dcb36
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2021-02-05 10:06:00 +0800

    That was way too hardcore

--- apply-patches.sh
+++ apply-patches.sh
@@ -16,16 +16,15 @@ for project in $(cd $patches/patches; echo *);do
 			continue
 		fi
 
-		if git apply --check $patch > /dev/null;then
+		if git apply --check $patch;then
 			git am $patch
 		elif patch -f -p1 --dry-run < $patch > /dev/null;then
 			#This will fail
 			git am $patch || true
-			patch -f -p1 < $patch > /dev/null
-			git add -u > /dev/null
-			git am --continue > /dev/null
+			patch -f -p1 < $patch
+			git add -u
+			git am --continue
 		else
-			git am $patch
 			echo ""
 			echo "Failed applying $patch"
 			echo ""
