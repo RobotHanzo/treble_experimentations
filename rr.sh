a92a882393489a194aa396255e1dec8acee7c83a
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2021-04-26 17:49:31 +0800

    no more slow uploader

--- rr.sh
+++ rr.sh
@@ -5,12 +5,6 @@ su="nosu"
 type="userdebug"
 scp_host='frs.sourceforge.net'
 images="release/$(date +%y%m%d)/*.img"
-# This section should be manually configured to match what it really is
-
-scp_remote_path=''
-scp_username=''
-scp_password=''
-
 
 # Use 4G for JVM memory to prevent Metalava from failing
 export _JAVA_OPTIONS='-Xmx4G'
@@ -25,10 +19,6 @@ function usage()
     echo "--gapps=$vanilla"
     echo "--su=$nosu"
     echo "--type=$userdebug"
-    echo "Auto uploader:"
-    echo "--remote-path=$scp_remote_path"
-    echo "--username=$scp_username"
-    echo "--password=$scp_password"
     echo ""
 }
 while [ "$1" != "" ]; do
@@ -51,15 +41,6 @@ while [ "$1" != "" ]; do
         --type)
             type=$VALUE
             ;;
-        --remote-path)
-            scp_remote_path=$VALUE
-            ;;
-        --username)
-            scp_username=$VALUE
-            ;;
-        --password)
-            scp_password=$VALUE
-            ;;
         *)
             echo "ERROR: unknown parameter \"$PARAM\""
             usage
@@ -77,24 +58,12 @@ echo "OpenGAPPS Implementation: $gapps"
 echo "Superuser: $su"
 echo "Build Type: $type"
 echo ""
-echo "Auto Uploader"
-echo ""
-echo "Upload to: $scp_remote_path"
-echo "With username: $scp_username"
-echo "With password: $scp_password"
-echo ""
 echo "================================"
 echo ""
 read -p "Press any key to proceed building Resurrection Remix Q"
 bash ../treble_experimentations/build-dakkar.sh -j $j rr-q arm-aonly-$gapps-$su-$type arm-ab-$gapps-$su-$type a64-aonly-$gapps-$su-$type a64-ab-$gapps-$su-$type arm64-aonly-$gapps-$su-$type arm64-ab-$gapps-$su-$type
-echo "================================="
+echo "=============="
 echo ""
-echo "Build succeeded, now uploading..."
+echo "Build finished"
 echo ""
-echo "================================="
-sshpass -p $scp_password scp $images $scp_username@$scp_host:$scp_remote_path
-echo "========================================================="
-echo ""
-echo "Upload finished, the building process has fully completed"
-echo ""
-echo "========================================================="
+echo "=============="
